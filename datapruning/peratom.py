538083e7f997aad3d38382cc9370012c0e58d0f9
IlgarBaghishov <baghishov@utexas.edu>
2024-10-08 23:52:31 -0400

    Created first parent classes for both data types

---
+++ datapruning/peratom.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+class perAtomData:
+
+    def __init__(self, n_configs):
+        self.n_configs = n_configs
+
+    def add_configuration(self):
+        pass
---
+++ datapruning/perconfiguration.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+class perConfigData:
+
+    def __init__(self, design_mat):
+        self.design_mat = design_mat
+
+    def calc_hat_matrix(self):
+        pass
